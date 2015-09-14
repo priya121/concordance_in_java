1343646617501298b9f4b9bf946e5971233c13c2
priya121 <priya121@github.com>
2015-09-14 13:46:47 +0100

    Made it possible to have multiple indicies.

--- src/priya/Concordance.java
+++ src/priya/Concordance.java
@@ -1,26 +1,19 @@
 package priya;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Concordance {
 
     public static void main(String args[]) {
-        findIndex("h");
+        findIndex("hi how are you?");
     }
 
-    public static HashMap<String, ArrayList<Integer>> findIndex(String input) {
-        String text = new String("Hi, how hare you, Hi?");
-        HashMap<String, ArrayList<Integer>> table = new HashMap<>();
-        ArrayList<Integer> array = new ArrayList<Integer>(0);
-
-        int i = text.indexOf(input);
-            while (i >= 0) {
-                array.add(i);
-                table.put(text.substring(i, i + input.length()), array);
-                i = text.indexOf(input, i + 1);
-            }
-        return table;
+    public static String findIndex(String text) {
+        HashMap<String, Integer> table = new HashMap<>();
+        String[] wordsString = text.split(" ");
+        String result = "";
+        table.put(wordsString[0], text.indexOf(wordsString[0]));
+        return (result + table);
     }
 }
 
